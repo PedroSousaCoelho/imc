860ede80127da78c36ffc69c39821b2f0121463e
PedroSousaCoelho <pedrosousacoelhoo@gmail.com>
2022-11-13 21:10:04 -0300

    2.0

--- teste.js
+++ teste.js
@@ -186,21 +186,64 @@
 // pedro.descrever()
 // vitor.descrever()
 
-class Carro { 
-    marca; 
-    cor;
-    gasto;
+// class Carro { 
+//     marca; 
+//     cor;
+//     gasto;
 
-    constructor(marca, cor, gasto){ 
-        this.marca = marca
-        this.cor = cor
-        this.gasto = gasto
+//     constructor(marca, cor, gasto){ 
+//         this.marca = marca
+//         this.cor = cor
+//         this.gasto = gasto
+//     }
+//     calcularGasto(distancia, precoCombustivel){
+//         return (distancia / this.gasto) * precoCombustivel
+//     }
+// }
+// const volks = new Carro('Volkswagen', 'Preto', 10) 
+// const ferrari = new Carro('Ferrari', 'vermelha', 1)
+// const res = ferrari.calcularGasto(300, 5)
+// console.log(res)
+
+class Pessoa { 
+    nome;
+    peso; 
+    altura
+
+    constructor(nome, peso, altura){ 
+        this.nome = nome
+        this.peso = peso 
+        this.altura = altura
     }
-    calcularGasto(distancia, precoCombustivel){
-        return (distancia / this.gasto) * precoCombustivel
+    //METODO
+    pesar() { 
+        const peso = this.peso
+        const altura = this.altura
+        const imc = calcularIMC(peso, altura)
+        console.log(`Pedro tem ${peso} kgs, sua altura ${altura} metros`)
+        console.log(classificarIMC(imc))
+    } 
+}
+
+const pedro = new Pessoa('Pedro S. Coelho', 51, 1.74)
+
+function calcularIMC(peso, altura){ 
+    const resIMC = peso / Math.pow(altura, 2)
+    console.log(resIMC)
+    return resIMC
+}
+
+function classificarIMC(imc){ 
+    if(imc < 18.5){ 
+        return'Abaixo do Peso'
+    } else if(imc >= 18.5 && imc < 25){ 
+        return'Peso Normal'
+    } else if(imc >= 25 && imc < 30){ 
+        return'Acima do Peso'
+    } else if(imc >= 30 && imc < 40){ 
+        return'Obeso'
+    } else { 
+        return'Obesidade grave'
     }
 }
-const volks = new Carro('Volkswagen', 'Preto', 10) 
-const ferrari = new Carro('Ferrari', 'vermelha', 1)
-const res = ferrari.calcularGasto(300, 5)
-console.log(res)
+pedro.pesar()
